1b72894fcc21921f100da9b5a9c0a1ba909a36b0
Juan Esteban Botello <juaneskill98@gmail.com>
2017-04-26 15:49:30 -0500

    Update Apuestas.py

--- Apuestas.py
+++ Apuestas.py
@@ -2,68 +2,29 @@ jug1 = 0
 jug2 = 0
 jug3 = 0
 lista = [[100,10,50],[1,30,1500],[200,5,30],["G","P","G"],["P","G","P"],["P","G","G"]]
+lista2 =[0,0,0]
+lista3=[0,1,2]
+f4= filter(lambda x:x, lista[0: int((len(lista)/2))])
+f5= filter(lambda x:x, lista[int((len(lista)/2)):(len(lista)-1)])
+#print f5[0][0]
+for x in lista3:
+    for y in lista3:
+        print f5[x][y]
+        print x,y
+        print lista2
+        if (f5[x][y] == "G"):
+            lista2[y] = lista2[y] + f4[x][y]
+        else:
+            lista2[y] = lista2[y] - f4[x][y]
 
-filtro = filter(lambda x: x, lista[3])
-filtroo = filter(lambda x: x, lista[4])
-filtrooo = filter(lambda x: x, lista[5])
-f1 = list(map(lambda x: x, lista[0]))
-f2 = list(map(lambda y: y, lista[1]))
-f3 = list(map(lambda z: z, lista[2]))
+print lista2
 
-if (filtro[0] == "G"):
-    jug1 = jug1 + f1[0]
-else:
-    jug1 = jug1 - f1[0]
-
-if (filtro[1] =="G"):
-    jug2 = jug2 + f1[1]
-else:
-    jug2 = jug2 - f1[1]
-    
-if (filtro[2] == "G"):
-    jug3 = jug3 + f1[2]
-else:
-    jug3 = jug3 - f1[2]
-
-
-
-if (filtroo[0] == "G"):
-    jug1 = jug1 + f2[0]
-else:
-    jug1 = jug1 - f2[0]
-
-if (filtroo[1] =="G"):
-    jug2 = jug2 + f2[1]
-else:
-    jug2 = jug2 - f2[1]
-
-if (filtroo[2] == "G"):
-    jug3 = jug3 + f2[2]
-else:
-    jug3 = jug3 - f2[2]
-
-
-
-if (filtrooo[0] == "G"):
-    jug1 = jug1 + f3[0]
-else:
-    jug1 = jug1 - f3[0]
-
-if (filtrooo[1] == "G"):
-    jug2 = jug2 + f3[1]
-else:
-    jug2 = jug2 - f3[1]
-
-if (filtrooo[2] == "G"):
-    jug3 = jug3 + f3[2]
-else:
-    jug3 = jug3 - f3[2]
-
-puntaje = [jug1, jug2, jug3]
-print puntaje
 
 f = lambda a,b: a if (a > b) else b
-print "Quien gano mas dinero: ",(reduce(f, puntaje))
+print "Quien gano mas dinero: ",(reduce(f, lista2))
 
 g = lambda c,d: c if (c < d) else d
-print "Quien perdio mas dinero: ",(reduce(g, puntaje))
+print "Quien perdio mas dinero: ",(reduce(g, lista2))
+print list(f4)
+print list(f5)
+
